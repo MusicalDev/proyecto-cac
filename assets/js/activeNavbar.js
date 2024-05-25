05723dea55976f9bb2fe1d7dddc014596251f36d
MusicalDev <ulisesoreste@gmail.com>
2024-05-25 01:02:33 -0300

    try to fix activeNavbar

--- assets/js/activeNavbar.js
+++ assets/js/activeNavbar.js
@@ -1,9 +1,21 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const currentPageURL = window.location.href;
+//     const navLinks = document.querySelectorAll('.nav-link');
+//     navLinks.forEach(link => {
+//         if (link.href === currentPageURL) {
+//             link.classList.add('active');
+//         }
+//     });
+// });
 document.addEventListener("DOMContentLoaded", function () {
-    const currentPageURL = window.location.href;
+    const currentPagePath = window.location.pathname; // Obtener solo la parte de la ruta de la URL actual
     const navLinks = document.querySelectorAll('.nav-link');
+
     navLinks.forEach(link => {
-        if (link.href === currentPageURL) {
+        const linkPath = new URL(link.href).pathname; // Obtener solo la parte de la ruta del enlace
+        if (linkPath === currentPagePath) {
             link.classList.add('active');
         }
     });
 });
+
