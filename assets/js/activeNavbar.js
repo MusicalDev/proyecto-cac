0750e0cf78eea8418bcdaabc8164723e2ba21c14
MusicalDev <ulisesoreste@gmail.com>
2024-07-06 13:05:48 -0300

    modificaciones Api - python anywhere y funciones

--- assets/js/activeNavbar.js
+++ assets/js/activeNavbar.js
@@ -7,15 +7,46 @@
 //         }
 //     });
 // });
-document.addEventListener("DOMContentLoaded", function () {
-    const currentPagePath = window.location.pathname;
-    const navLinks = document.querySelectorAll('.nav-link');
-    navLinks.forEach(link => {
-        // Compara la ruta de la página actual con el atributo href de cada enlace
-        if (link.getAttribute('href') === currentPagePath) {
-            link.classList.add('active');
-        }
-    });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     const currentPagePath = window.location.pathname;
+//     const navLinks = document.querySelectorAll('.nav-link');
+//     navLinks.forEach(link => {
+//         // Compara la ruta de la página actual con el atributo href de cada enlace
+//         if (link.getAttribute('href') === currentPagePath) {
+//             link.classList.add('active');
+//         }
+//     });
+// });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPageURL = window.location.href;
+    const navLinks = document.querySelectorAll('.nav-link');
+    const misProductosLink = document.querySelector('.mis-productos-link');
+
+    console.log("Current Page URL:", currentPageURL);
+    console.log("Mis Productos Link:", misProductosLink);
+
+    // Marcar "Mis productos" como activo si la URL contiene cualquiera de las siguientes páginas
+    if (
+        currentPageURL.includes('altas.html') ||
+        currentPageURL.includes('listado.html') ||
+        currentPageURL.includes('listadoEliminar.html') ||
+        currentPageURL.includes('modificaciones.html') ||
+        currentPageURL.includes('MisProductos.html')
+    ) {
+        if (misProductosLink) {
+            misProductosLink.classList.add('active');
+            console.log("Mis productos link marked as active");
+        } else {
+            console.log("Mis productos link not found");
+        }
+    } else {
+        navLinks.forEach(link => {
+            if (link.href === currentPageURL) {
+                link.classList.add('active');
+                console.log(`Link ${link.href} marked as active`);
+            }
+        });
+    }
+});
 
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -4,7 +4,7 @@ let Navbar = `
 <ul class="navegacion">
   <li><a href="/" class="nav-link">Home</a></li>
   <li><a href="/Swap.html" class="nav-link">Swap</a></li>
-  <li><a href="/MisProductos.html" class="nav-link">Mis productos</a></li>
+  <li><a href="/MisProductos.html" class="nav-link mis-productos-link">Mis productos</a></li>
   <li><a href="/Perfil.html" class="nav-link">Perfil</a></li>
   <li><a href="/Login.html" class="nav-link">Login</a></li>
   <li><a href="/Contacto.html" class="nav-link">Contacto</a></li>
